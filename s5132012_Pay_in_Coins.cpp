23b76054e0342a0d6630674e63433146d622b71e
Tom-Sheiles <36983461+Tom-Sheiles@users.noreply.github.com>
2019-04-02 15:32:12 +1000

    updated main file

--- s5132012_Pay_in_Coins.cpp
+++ s5132012_Pay_in_Coins.cpp
@@ -5,9 +5,28 @@
 
 using namespace std;
 
+void parseNumber(string line){
+    cout << line << endl;
+}
+
+void calculate(string *inputs, int nInputs){
+    
+    for(int i = 0; i < nInputs; i++){
+        parseNumber(inputs[i]);
+    }
+    
+}
+
+
 int main(int argc, char **argv){
     
-    ifstream inputFile("input.txt");
+    string filePath;
+    cout << "Enter the file path for \"input.txt\": ";
+    cin >> filePath;
+    filePath.append("\\input.txt");
+    cout << "Opening: " << filePath << endl;
+    
+    ifstream inputFile(filePath.c_str());
     string lines;
     string inputs[100];
 
@@ -24,5 +43,7 @@ int main(int argc, char **argv){
         cout << "Could not Find \"input.txt\"" << endl;
         exit(1);
     }   
+
+    calculate(inputs, i);
  
 }
