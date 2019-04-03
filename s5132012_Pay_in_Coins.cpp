8f7c3c88a4559ab51335028efccb4bc5c6f19b1a
Tom-Sheiles <36983461+Tom-Sheiles@users.noreply.github.com>
2019-04-03 12:38:11 +1000

    update main

--- s5132012_Pay_in_Coins.cpp
+++ s5132012_Pay_in_Coins.cpp
@@ -2,17 +2,66 @@
 #include <iostream>
 #include <string>
 #include <fstream>
+#include <sstream>
 
 using namespace std;
 
-void parseNumber(string line){
-    cout << line << endl;
+void parseNumber(string line, int *value, int *beginRange, int *endRange){
+    
+    int n = 1;
+    int i = 0;
+    int values[3];
+    std::stringstream stream(line);
+    
+    //initialize values to -1
+    for (int j = 0; j < 3; j++){    
+        values[j] = -1;
+    }
+    
+    //count amount of numbers in line
+    for(int i = 0; i < int(line.length()); i++){
+        if(isspace(line[i])){
+            n++;
+        }
+    }
+
+    //assign each number to an int array
+    for(i = 0; i < n; i++){
+        stream >> values[i];
+    }
+    
+    //change the pointer values
+    *value = values[0];
+    *beginRange = values[1];
+    *endRange = values[2];
+    return;
+    
+}
+
+int* calculatePrimes(int value, int min, int max){
+    
+    int *primes;
+    
+    
+    return primes;
+}
+
+void countCoins(int value, int beginRange, int endRange){
+    
+    int *primeArray;
+    primeArray = calculatePrimes(value, beginRange, endRange);
+    
 }
 
 void calculate(string *inputs, int nInputs){
     
+    int beginRange = 0;
+    int endRange = 0;
+    int value = 0;
+    
     for(int i = 0; i < nInputs; i++){
-        parseNumber(inputs[i]);
+        parseNumber(inputs[i], &value, &beginRange, &endRange);
+        countCoins(value, beginRange, endRange);
     }
     
 }
